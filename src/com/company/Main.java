2037b50afa27ce3fc27cba3843c026daa87e658e
Tasnim <nowshintasnim@gmail.com>
2018-07-21 14:34:52 +0600

    second commit: added a new println()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello from github");
+        System.out.println("This is my second commit!");
+
     }
 }
